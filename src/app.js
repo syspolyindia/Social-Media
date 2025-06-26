07dfb5ae9b6d1e570aab1ca79a1c2e3e98d1ef57
syspoly <syspolyindia@gmail.com>
2025-06-26 09:55:25 +0530

    commit-2

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json({limit: "16kb"})) // any data that is being sent to us in JSON format would have a limit of 16kb, data of larger size is not permitted.
 
-app.use(express.urlencoded({extended:true, limit:"16kb"})); //to parse the request body (if the request sent is in such format that it needs to be parsed) into javascript object.
+app.use(express.urlencoded({extended:true, limit:"16kb"})); //To parse the request body (if the request sent is in such format that it needs to be parsed) into javascript object.
 
 app.use(express.static(path.join(__dirname, '../public')));
 
